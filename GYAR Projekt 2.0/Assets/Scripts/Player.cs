101a4756ab33f54f2e039896858f15f7389e60c5
Squidofpixels <hampus.mollerstromolsson@elev.ga.lbs.se>
2021-01-13 10:28:17 +0100

    Healthbar + script
    Healthbar objekt och ett skript till det.

---
+++ GYAR Projekt 2.0/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 4;
+    public int currentHealth;
+
+    public Healthbar healthbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthbar.SetHealth(currentHealth);
+    }
+}
